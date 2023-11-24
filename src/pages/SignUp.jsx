c627d0658299a1db73bd91edb532b74f63e7629b
Arun kumar <arunkumarjr1999@gmail.com>
2023-11-24 20:54:14 +0530

    Create a sign-in page functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -36,7 +36,7 @@ const SignUp = () => {
       }
       setLoading(false);
       setError(null);
-      navigate('/sign-in')
+      navigate('/sign-in');
       
     } catch (error) {
       setLoading(false);
